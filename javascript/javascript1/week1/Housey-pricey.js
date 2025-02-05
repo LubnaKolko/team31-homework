756bcf5bb205725881cd01b766ad1e1874a5eca9
Lubnakolko <loubna88812@icloud.com>
2025-02-05 19:23:14 +0100

    Housey-pricey.js with initial content

---
+++ javascript/javascript1/week1/Housey-pricey.js
@@ -0,0 +1,9 @@
+console.log("Housey-pricey exercise");
+
+const houseWidth = 8;
+const houseDepth = 10;
+const houseHeight = 10;
+const Gardensize = 100;
+const ActualPrice = 2500000;
+
+let volume = houseWidth * houseDepth * houseHeight;
