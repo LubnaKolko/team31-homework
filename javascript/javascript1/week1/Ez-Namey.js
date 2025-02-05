d30128998805a802965a9bbc2e9b1dc747a8b513
Lubnakolko <loubna88812@icloud.com>
2025-02-05 19:49:57 +0100

    index.html with initial content

---
+++ javascript/javascript1/week1/Ez-Namey.js
@@ -0,0 +1,9 @@
+console.log(Ez-Namey-exercise);
+
+
+const firstWords = ["Easy", "Awesome", "Corporate", "Innovative", "Creative", "Smart", "Intelligent", "Clever", "Brilliant", "Genius"];
+const secondWords = ["Company", "Business", "Corporation", "Innovation", "Creativity", "Solutions", "Technology", "Intelligence", "Innovations", "Genius"];
+
+const randomNumber = Math.floor(Math.random() * 10);
+const startupName = firstWords[randomNumber] + " " + secondWords[randomNumber];
+const nameLength = startupName.length;
